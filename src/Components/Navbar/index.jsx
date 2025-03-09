08143bffcb19e88373ec02b107380da053a39f22
Favian David Martinez Martin (Externo) (Colombia) <ext_fmartinez@mallplaza.com>
2025-03-08 19:08:11 -0500

    Se guardan cambios

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { ShoppingBagIcon, Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
-
+import { HomeIcon } from "@heroicons/react/24/solid";
 import { ShoppingCartContext } from "../../Context";
 import "./navbar.css";
 
